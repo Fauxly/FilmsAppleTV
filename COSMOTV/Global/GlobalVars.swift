9ebe42920bc85c241d2305ab44495b4c03ced941
Ihor Didenko <tailormoon@icloud.com>
2022-09-24 20:58:21 +0300

    Update GlobalVars.swift

--- COSMOTV/Global/GlobalVars.swift
+++ COSMOTV/Global/GlobalVars.swift
@@ -3,7 +3,7 @@ import UIKit
 
 class UDvars {
     let urlString: String = "https://videocdn.tv/api/",
-        token: String = "?api_token=3FmXURIs0EGMSuPyGitpJFjRpFCfPHXu",
+        token: String = "?api_token=PVCQ3dE3lGbW2oa8Rrk3QS0R9yjdHKLy",
         imageCache = NSCache<NSString, UIImage>()
 }
 
